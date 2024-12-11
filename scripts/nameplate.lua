02e9f0006726c3a4e0df3d66367c1a582126b0cb
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2024-12-11 19:26:25 +0100

    fancy nameplate hover

--- scripts/nameplate.lua
+++ scripts/nameplate.lua
@@ -1,5 +1,31 @@
+---formats text
+---@param str string
+---@param startColor string?
+---@return table
+local function formatText(str, startColor)
+   local json = toJson({{text = str, color = startColor}})
+   json = json:gsub('${(.-)}', '"},{"color":"%1","text":"')
+   return parseJson(json)
+end
+
 nameplate.ALL:setText(toJson{
-   "${badges}:witch_hat: ",
+   {
+      text = "${badges}:witch_hat: ",
+      hoverEvent = {action = "show_text", value = {
+         avatar:getEntityName(),
+         {text = ' ❄', color = '#84ebff'},
+         '\n',
+         formatText(avatar:getUUID():gsub('%-', '${#84ebff}-${#ffffff}'), '#ffffff'),
+         formatText([[
+
+┌──┐
+││    │     ${#84ebff}Made by${#4769d7}:${#ffffff}
+││    │     ${#84ebff}AuriaFoxGirl${#ffffff}
+├──┤
+││    │
+└──┘]])
+      }},
+   },
    {
       text = '${name}',
       color = '#84ebff'
